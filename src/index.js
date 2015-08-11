6182ceed8eb8b8b2254f537d389c590fa2d53d0b
Josh Beam <joshua.a.beam@gmail.com>
2015-08-11 13:44:53 -0700

    adds #isFulfilled static method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 angular.module('chQAllSettled', []).config(function($provide) {
   $provide.decorator('$q', function($delegate) {
     var $q = $delegate;
+    var fulfilled = 'fulfilled';
+    var rejected = 'rejected';
+
     $q.allSettled = function(promises) {
       return $q.all(promises.map(function(promise) {
         return promise.then(function(value) {
-          return { state: 'fulfilled', value: value };
+          return { state: fulfilled, value: value };
         }, function(reason) {
-          return { state: 'rejected', reason: reason };
+          return { state: rejected, reason: reason };
         });
       }));
     };
+
+    $q.isFulfilled = function(promises) {
+      return promises.reduce(function(agg, c) {
+        return c.state === fulfilled ? agg.concat([c.value]) : agg;
+      }, []);
+    };
+
     return $q;
   });
 });
